9436b03870a9557ab6c222d8832787b41aa2f1ff
FreezingBP <you@example.com>
2020-07-15 12:22:50 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CustomCamera
+自定义相机实现录制视频
---
+++ app/src/main/java/com/liuxiaozhu/customcamera/MainActivity.java
@@ -0,0 +1,32 @@
+package com.liuxiaozhu.customcamera;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+    private static final int REQUEST_PERMISSION = 0x1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO}, REQUEST_PERMISSION);
+            }
+        }
+    }
+
+    public void onClick(View view) {
+        startActivity(new Intent(this, CameraActivity.class));
+    }
+}
